68f2ec3d58e892916b488a6b36feae9cb3257730
kristopherdelane <kdstev@gmail.com>
2017-02-28 09:16:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -180,8 +180,10 @@ var g = wrapper.selectAll("g.group")
 	.on("mouseout", fade(opacityDefault));
 
 g.append("path")
-	.style("stroke", function(d,i) { return (Names[i] === "" ? "none" : "#00A1DE"); })
-	.style("fill", function(d,i) { return (Names[i] === "" ? "none" : "#00A1DE"); })
+	.style("stroke", function(d,i) { return (Names[i] === "" ? "none" : "#800000"); })
+	.style("fill", function(d,i) { return (Names[i] === "" ? "none" : "#800000"); })
+//	.style("stroke", function(d,i) { return (Names[i] === "" ? "none" : "#00A1DE"); })
+//	.style("fill", function(d,i) { return (Names[i] === "" ? "none" : "#00A1DE"); })
 	.style("pointer-events", function(d,i) { return (Names[i] === "" ? "none" : "auto"); })
 	.attr("d", arc)
 	.attr("transform", function(d, i) { //Pull the two slices apart
